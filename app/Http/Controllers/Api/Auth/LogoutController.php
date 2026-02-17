c4b92716a3b7a73737ec27dc40b2dea1bef25dec
TasminskyiRuslan <tasminskyi.ruslan@gmail.com>
2026-02-17 18:53:58 +0000

    Added swagger documentation for LogoutController

--- app/Http/Controllers/Api/Auth/LogoutController.php
+++ app/Http/Controllers/Api/Auth/LogoutController.php
@@ -6,9 +6,28 @@ use App\Actions\Auth\LogoutUserAction;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use OpenApi\Attributes as OA;
+use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 
 class LogoutController extends Controller
 {
+    #[OA\Delete(
+        path: '/auth/logout',
+        description: 'Revoke the authentication token for the authenticated user.',
+        summary: 'Logout current device',
+        security: [['sanctum' => []]],
+        tags: ['Auth'],
+        responses: [
+            new OA\Response(
+                response: SymfonyResponse::HTTP_NO_CONTENT,
+                description: 'User logged out'
+            ),
+            new OA\Response(
+                response: SymfonyResponse::HTTP_UNAUTHORIZED,
+                description: 'Unauthorized'
+            )
+        ]
+    )]
     /**
      * Handle the incoming request to logout the authenticated user.
      *
